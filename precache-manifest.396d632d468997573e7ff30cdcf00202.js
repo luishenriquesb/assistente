e0d8e9508220c197421f24a33a439055125df2b6
Luis Brito <luishenrique.sb@gmail.com>
2020-09-30 20:56:31 -0300

    Updates

---
+++ precache-manifest.396d632d468997573e7ff30cdcf00202.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fd569a7afc649ae7c408ad98cdd0b70",
+    "url": "/assistente/index.html"
+  },
+  {
+    "revision": "9679cc7d775e29168ff9",
+    "url": "/assistente/static/css/2.e961af42.chunk.css"
+  },
+  {
+    "revision": "3880e1ad33c43989e544",
+    "url": "/assistente/static/css/main.67f9b2d2.chunk.css"
+  },
+  {
+    "revision": "9679cc7d775e29168ff9",
+    "url": "/assistente/static/js/2.199d0df3.chunk.js"
+  },
+  {
+    "revision": "cea9dec09d1dae161dfe6d24c63cf349",
+    "url": "/assistente/static/js/2.199d0df3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3880e1ad33c43989e544",
+    "url": "/assistente/static/js/main.6f1cda08.chunk.js"
+  },
+  {
+    "revision": "bd3a93a7cda8363ad00c",
+    "url": "/assistente/static/js/runtime-main.d43f6e3d.js"
+  }
+]);
